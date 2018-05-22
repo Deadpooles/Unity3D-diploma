d4f58b31573c9db18df087f59a6e9d7ef380da75
Nick Ilnitsky <deadpooles201@gmail.com>
2018-05-22 20:03:06 +0300

    Clear Project v 0.5

---
+++ diploma_first_level/Assets/VW_ActionRPG_AssetPack/Scripts/LoadScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using HutongGames.PlayMaker;
+using System;
+
+public class LoadScene : MonoBehaviour {
+
+	public int level;
+
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log ("Loading Level");
+		Application.LoadLevel(level);
+	}
+}
